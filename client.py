5d428a6571e7a2b00aa87b60ee7be5f5844620cb
agba-coder <demiladebamgboye@gmail.com>
2023-10-16 09:49:03 +0100

    FIXED: addWord() API errors and bugs, ADDED: JSONResponse status code and content

--- client.py
+++ client.py
@@ -17,14 +17,14 @@ import requests
 
 def searchAPI():
     print()
-    word = input("Enter a word: ") # prompts user for a word
-    api_url = f"http://127.0.0.1:8000/word-search?q={word}" # API url - "127.0.0.1:8000" is used to test on loacl machine
+    word = input("Enter a word: ").lower() # prompts user for a word
+    api_url = f"http://127.0.0.1:8000/word-search?q={word.lower()}" # API url - "127.0.0.1:8000" is used to test on loacl machine
     
     # fetchs data from HTTP Response
     response = requests.get(api_url)
     data = response.json()
-    # print(data)
-    # print(response.json(), response.status_code, response.headers)
+    
+    # print(response.json(), response.status_code, response.headers)  
     for key in data:
         print(f"{key} - {data[key]}")
         
@@ -32,34 +32,43 @@ def searchAPI():
 def addWordAPI():
     print()
     
-    word = input("Type word: ")
-    meaning = input("Enter word meaning: ")
-    
-    content = {"word": word, "meaning": meaning }
-    # print(content)
+    word = input("[PROMPT] ==> Type word: ").lower() # coverts string to all lowercase
+    meaning = input("[PROMPT] ==> Enter word meaning: ")
+     
+    content = {"word": word.lower(), "meaning": meaning }
     
     api_url = f"http://127.0.0.1:8000/add-word/" # API url - "127.0.0.1:8000" is used to test on loacl machine
     
     try:     
         # sends POST request to add word and meaning
         response = requests.post(api_url, json=content)
+        
         # check HTTP response status code
         if response.status_code == 200:
-            print("Word and meaning added to Database successfully")
+            print()
+            print("[SUCCESS] ==> Word and meaning added to Database successfully")
+            
+        elif response.status_code == 201:
+            print("[INFO] ==> Word already exist in Database")
+            
         else:
-            print(f"Failed to add word and meaning. Status code: {response.status_code}")
+            print(f"[ERROR] ==> Failed to add word and meaning. Status code: {response.status_code}")
     
     except requests.exceptions.RequestException as e:
-        print(f"Oops!. An error occured: {e}")
+        print(f"[ERROR] ==> Oops!. An error occured: {e}")
+        
+
+def removeAPI():
+    pass
         
 
 def main():
     print()
     print("WARNING: This program/script is only added or written to test and consume our Dictionary FastAPI.. Softwares like Postman can used instead of this piece of code to test out this API and access more functionalities.\n Also, the JSON file that conatains words and their meaning aren't complete i.e doesn't contain all words in English. it's just created to test out this software. You can get your own word bank or data from a reliable source! \nTherefore, this pratice is not the best way to test out APIs!")
     print("\n")
-    print("1. Search for a word")
-    print("2. Add a word")
-    print("3. delete a word")
+    print("[1]. Search for a word")
+    print("[2]. Add a word")
+    print("[3]. delete a word")
     
     try:
         res = int(input("Choose option: "))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from fastapi.responses import JSONResponse
 from typing import Optional
 from pydantic import BaseModel
 
@@ -6,8 +7,7 @@ import json
 
 app = FastAPI()
 
-# data = json.loads(open("simple_english_dictionary.json", "r")) #
-with open("simple_english_dictionary.json", "r") as json_file:
+with open("simple_english_dictionary.json", "r+") as json_file:
     data = json.load(json_file)
     
 
@@ -29,34 +29,43 @@ async def home():
     data(word search meaning) or error messages
 '''
 @app.get("/word-search")
-async def wordSearch(*, q: Optional[str] = None):
+async def wordSearch(*, query: Optional[str] = None):
     # loop through words
     for word in data:
         # validate word search
-        if q in data:
-            print(q)
+        if query in data:
             # concantenate word and it meaning into a dict/json format
-            x = {q : data[q]}
+            x = {query : data[query]}
             # returns data 
-            return x
+            return JSONResponse(content=x, status_code=200)
         else:
-            return {"Oops": "Word not found!"}
+            error = {"Oops": "Word not found!"}
+            return JSONResponse(content=error, status_code=404)
 
 
 @app.post("/add-word/")
 async def addWord(word_item: dict):
-    
     # parsing json response
     word = word_item["word"]
     meaning = word_item["meaning"]
     
     # checks if word exists 
-    if word_item["word"] in data:
-        return {"INFO": "Word already exist in Database!"}
+    if word in data.keys():
+        return JSONResponse(content="Word already exist in Database", status_code=201)
     
-    # save word and meaning to DB
-    data[word] = meaning
-    return {"INFO": "Word added to Database!"}
-
-
-    
+    else:
+        # data to be added as a new key-value pair
+        new_data = {word:meaning}
+        
+        # read the existing JSON data from the file in read mode ('r')
+        with open("simple_english_dictionary.json", "r") as file:
+            db_file = json.load(file)
+            
+        # add the new key-value pair to the exxisting data
+        db_file.update(new_data)
+        
+        # write the modified data back to the file
+        with open("simple_english_dictionary.json", "w") as file:
+            json.dump(db_file, file, indent=4)
+            
+        return JSONResponse(content="Word added to Database!", status_code=200) 
